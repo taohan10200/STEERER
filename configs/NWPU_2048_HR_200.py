dc1a1bc0e09c9f28744f35e14503049a8dd08c4a
taohan10200 <taohan10200@163.com>
2023-11-09 16:22:14 +0800

    fix transcos link

--- configs/NWPU_2048_HR_200.py
+++ configs/NWPU_2048_HR_200.py
@@ -13,7 +13,7 @@ network = dict(
     gau_kernel_size = 15,
     baseline_loss = False,
     pretrained_backbone="../PretrainedModels/hrnetv2_w48_imagenet_pretrained.pth",
-    # '/mnt/petrelfs/hantao/HRNet-Semantic-Segmentation/exp/imagenet/CCFBackbone_hrt_base/ccformer_b_2022-08-16-14-59/Ep_142_acc1_75.12800201416016.pth',
+
 
     head = dict(
         type='CountingHead',
@@ -79,8 +79,7 @@ train = dict(
     extra_epoch=0,
     extra_lr = 0,
     #  RESUME: true
-    resume_path=None,#'./exp/NWPU/MocHRBackbone_hrnet48/mocHR_small_2022-09-19-20-52/', #'/mnt/petrelfs/hantao/HRNet-Semantic-Segmentation/exp/NWPU/MocHRBackbone_hrnet48/mocHR_small_2022-09-19-01-39/',#'/mnt/petrelfs/hantao/HRNet-Semantic-Segmentation/exp/NWPU/MocHRBackbone_hrnet48/mocHR_small_2022-09-18-10-29/', #'/mnt/petrelfs/hantao/HRNet-Semantic-Segmentation/exp/NWPU/MocHRBackbone_hrnet48/mocHR_small_2022-09-15-15-22/', #'/mnt/petrelfs/hantao/HRNet-Semantic-Segmentation/exp/NWPU/MocHRBackbone_hrnet48/mocHR_small_2022-09-13-16-28/', # '/mnt/petrelfs/hantao/HRNet-Semantic-Segmentation/exp/NWPU/MocHRBackbone_hrnet48/mocHR_small_2022-09-09-21-07/', #'./exp/NWPU/MocHRBackbone_hrnet48/mocHR_small_2022-09-06-20-35/', #'/mnt/petrelfs/hantao/HRNet-Semantic-Segmentation/exp/NWPU/MocHRBackbone_hrnet48/mocHR_small_2022-09-06-13-03/', #'./exp/NWPU/MocHRBackbone_hrnet48/mocHR_small_2022-09-04-19-21/', #'./exp/NWPU/MocBackbone_moc_small/moc_small_2022-08-31-14-41/', #'./exp/NWPU/seg_hrnet/hrt_small_2022-08-07-23-15/',
-    # './exp/NWPU/seg_hrnet/seg_hrnet_w48_2022-05-27-15-03'
+    resume_path=None,
     flip=True,
     multi_scale=True,
     scale_factor=16,
@@ -98,7 +97,7 @@ test = dict(
     flip_test=False,
     multi_scale=False,
     # './exp/NWPU/seg_hrnet/seg_hrnet_w48_nwpu_2022-06-03-23-12/Ep_138_mae_45.79466183813661_mse_116.98580130706075.pth'
-    model_file= './exp/NWPU/MocHRBackbone_hrnet48/NWPU_HR_2022-10-08-00-55/Ep_482_mae_73.72708279859529_mse_351.70288575101375.pth', #'./exp/NWPU/seg_hrnet/seg_hrnet_w48_2022-06-03-23-12/Ep_280_mae_54.884169212251905_mse_226.06904272422108.pth'
+    model_file= '',
 )
 
 CUDNN = dict(
